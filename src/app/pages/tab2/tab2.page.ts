3f9188ae161ea9fc5278e20cdf209a122e62557f
jesus <imangelo@hotmail.es>
2019-04-11 22:57:00 +0200

    mostrar registro en el historial

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { DataLocalService } from 'src/app/services/data-local.service';
+import { Registro } from 'src/app/models/registro.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  constructor(public dataLocal: DataLocalService){}
+  enviarCorreo(){
+    console.log("neviando correo");
+  }
+  abrirRegistro(registro: Registro){
+    console.log("registro", registro);
+  }
+}
