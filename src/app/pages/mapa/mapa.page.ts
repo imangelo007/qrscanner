eff7856c879e6a545dc8ed8fa4c57cefee2b74db
jesus <imangelo@hotmail.es>
2019-04-14 00:39:23 +0200

    edificio 3d y marcadores

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -13,18 +13,63 @@ export class MapaPage implements OnInit, AfterViewInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    let geo:any = this.route.snapshot.paramMap.get('geo');
+    let geo: any = this.route.snapshot.paramMap.get('geo');
     geo = geo.substr(4);
     geo = geo.split(',');
     this.lat = Number(geo[0]);
     this.lng = Number(geo[0]);
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiamVzdXMwMDciLCJhIjoiY2p1ZzFvcWk3MGd2aTN5cXI3eGdwYXN2ZSJ9.l4elnAUIqlmVkRb98EMQeg';
     var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [ this.lng, this.lat],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -17.6,
+      container: 'map'
+    });
+
+    map.on('load', () => {
+      new mapboxgl.Marker().setLngLat([this.lng, this.lat]).addTo(map);
+      // Insert the layer beneath any symbol layer.
+      map.resize();
+      var layers = map.getStyle().layers;
+
+      var labelLayerId;
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      }
+
+      map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+          'fill-extrusion-color': '#aaa',
+
+          // use an 'interpolate' expression to add a smooth transition effect to the
+          // buildings as the user zooms in
+          'fill-extrusion-height': [
+            "interpolate", ["linear"], ["zoom"],
+            15, 0,
+            15.05, ["get", "height"]
+          ],
+          'fill-extrusion-base': [
+            "interpolate", ["linear"], ["zoom"],
+            15, 0,
+            15.05, ["get", "min_height"]
+          ],
+          'fill-extrusion-opacity': .6
+        }
+      }, labelLayerId);
     });
   }
 }
